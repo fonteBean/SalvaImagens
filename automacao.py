7f4c4873796f00c4f2b123fb908ff714c9914583
fonteBean <n9ckbean@gmail.com>
2024-09-11 11:52:29 -0300

    Alterações da Primeira aula

--- automacao.py
+++ automacao.py
@@ -1,1 +1,20 @@
-text = "hello world"
+#Configurações básicas
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+service = Service(ChromeDriverManager().install())
+navegador = webdriver.Chrome(service=service)
+navegador.get("https://pages.hashtagtreinamentos.com/inscricao-minicurso-python-automacao-org?origemurl=hashtag_yt_org_minipython_8AMNaVt0z_M")
+
+#Main
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[1]/div/input').send_keys("Nicolas")
+
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[2]/div/input').send_keys("n9ckbean@gmail.com")
+
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/div[1]/div/div[3]/div/input').send_keys("22997864028")
+navegador.find_element('xpath',
+                       '//*[@id="section-10356508"]/section/div[2]/div/div[2]/form/button').click()
